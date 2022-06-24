fa22daee83f0b795d564c1b0c37f004fa117cc68
Khalid Benlyazid <kbenlyaz@e1r6p4.1337.ma>
2022-06-24 13:02:49 +0100

    start working with mongoDb

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/controller.error');
-const sequelize = require('./util/database').sequelize
 
 const Product = require('./models/model.product')
 const User = require('./models/model.user')
@@ -25,68 +24,25 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) =>{ //? add user to req middelware
-    User.findByPk(1)
-    .then(user =>{
-        req.user = user
-        next()
-    })
-    .catch(err =>{
-        console.log('error in finding user 1 ')
-        console.log(err);
-    })
-})
+// app.use((req, res, next) =>{ //? add user to req middelware
+//     User.findByPk(1)
+//     .then(user =>{
+//         req.user = user
+//         next()
+//     })
+//     .catch(err =>{
+//         console.log('error in finding user 1 ')
+//         console.log(err);
+//     })
+// })
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
-User.hasMany(Product, {
-    constraints: true,
-    onDelete: 'CASCADE'
-})
-User.hasOne(Cart)
-// Cart.belongsTo(User)
-Cart.belongsToMany(Product, {through : CartItem})
-Product.belongsToMany(Cart, {through : CartItem})
-Order.belongsTo(User)
-User.hasMany(Order)
-Order.belongsToMany(Product, {through: OrderItem})
 
+app.listen(3000);
 
-let _user;
-sequelize.sync()
-// sequelize.sync({ force: true })
-    .then(res => {
-        return User.findByPk(1);
-    })
-    .then(user =>{
-        if (!user){
-            return User.create({name : 'root', mail : 'root.@admin.shop.ma'})
-        }
-
-        else{
-            return Promise.resolve(user)
-        }
-    })
-    .then(user =>{
-        _user = user
-        console.log("User has been inserted")
-        return user.getCart()
-    })
-    .then(cart =>{
-        if (!cart)
-            return _user.createCart()
-        return Promise.resolve(cart) //> serch for Promiss.resolve() for more info
-    })
-    .then(() =>{
-        console.log("Cart has been created")
-        app.listen(3000);
-    })
-    .catch(err => {
-        console.log("error is ")
-        console.log(err);
-    })
 
 
 
--- util/database.js
+++ util/database.js
@@ -1,75 +1,13 @@
-const mysql = require('mysql2')
-
-const Sequelize = require('sequelize')
-
-const sequelize = new Sequelize('shop', 'root', 'root', {
-	dialect: 'mysql',
-	host: 'localhost'
-})
-
-const pool = mysql.createPool({
-	host: "localhost",
-	user: "root",
-	password: "root",
-	database: "shop"
-
-});
-//> learn more about defferent between connection and pool
-
-const createIfNotExist = () => {
-	let _pool = mysql.createPool({
-		host: "localhost",
-		user: "root",
-		password: "root",
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+const nodeUserPassword = '3oMAodrz4650SFOQ'
+const url = `mongodb+srv://node_user:${nodeUserPassword}@cluster0.xohjs8d.mongodb.net/?retryWrites=true&w=majority`
+const connectToMongo = (callBack) =>{
+	MongoClient.connect(url)
+	.then(client => {
+		console.log("connected\n\n")
+		callBack(client)
 	})
-	_pool.promise().execute('CREATE DATABASE IF NOT EXISTS shop')
-		.then(msg => {
-			console.log('DATABASE CREATED....')
-			// console.log(msg)
-			_pool = mysql.createPool({
-				host: "localhost",
-				user: "root",
-				password: "root",
-				database: "shop"
-			})
-			_pool.promise().execute('CREATE TABLE IF NOT EXISTS products (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL UNIQUE, title VARCHAR(255) NOT NULL, price DOUBLE NOT NULL, description VARCHAR(255) NOT NULL, imageUrl VARCHAR(255) NOT NULL)')
-				.then(msg => {
-					console.log('TABLE CREATED....')
-					// console.log(msg)
-				})
-				.catch(err => {
-					console.log('ERROR IN CRIEATING TABLE....')
-					console.log(err)
-				})
-		})
-		.catch(err => {
-			console.log('ERROR IN CRIEATING DATABSE....')
-			console.log(err)
-		})
+	.catch(err => console.log("err is \n\n" + err))
 }
-
-module.exports = {
-	db_pool: pool.promise(),
-	createIfNotExist: createIfNotExist,
-	sequelize: sequelize
-
-}
-
-
-
-
-/*                CREATE DATABASE
-
-CREATE DATABASE IF NOT EXISTS shop;
-*/
-
-/*                  CREATE TABLE
-CREATE TABLE IF NOT EXISTS products (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL UNIQUE, title VARCHAR(255) NOT NULL, price DOUBLE NOT NULL, description VARCHAR(255) NOT NULL, imageUrl VARCHAR(255) NOT NULL);
-*/
-
-/*                  INSERT DATA
-INSERT INTO products (title, price, description, imageURL) VALUES ('product_0', '120', 'This is a simple description of a pproduct', 'image_URL');
-*/
-
-
-
+// 3oMAodrz4650SFOQ
